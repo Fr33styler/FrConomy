ed2f62689d592f2cf0667344d3c66ac611cc7642
Fr33stylerRO <Fr33stylerMC@gmail.com>
2021-02-23 21:02:01 +0200

    Initial commit

---
+++ src/main/java/ro/fr33styler/frconomy/command/Money.java
@@ -0,0 +1,67 @@
+package ro.fr33styler.frconomy.command;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import ro.fr33styler.frconomy.FrConomy;
+import ro.fr33styler.frconomy.account.Account;
+
+public class Money implements CommandExecutor {
+
+    private FrConomy main;
+
+    public Money(FrConomy main) {
+        this.main = main;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String name, String[] args) {
+        if (args.length == 0) {
+            if (sender.hasPermission("frconomy.money")) {
+                if (sender instanceof Player) {
+                    Player p = (Player) sender;
+                    Account account = main.getAccounts().getAccount(p);
+                    p.sendMessage(main.getMoney().replace("%money%", main.formatCurrency(account.getBalance())));
+                    return true;
+                } else {
+                    sender.sendMessage("§cInvalid arguments! Please use /money <name>");
+                }
+            } else {
+                sender.sendMessage(main.getPermission());
+            }
+        } else if (args.length == 1) {
+            if (args[0].equals("toggle")) {
+                if (sender instanceof Player) {
+                    Player p = (Player) sender;
+                    Account account = main.getAccounts().getAccount(p);
+                    account.setReceive(!account.canReceive());
+                    if (account.canReceive()) {
+                        p.sendMessage(main.getMoneyToggleOn());
+                    } else {
+                        p.sendMessage(main.getMoneyToggleOff());
+                    }
+                    return true;
+                } else {
+                    sender.sendMessage("§cInvalid arguments! Please use /money <name>");
+                }
+            } else if (sender.hasPermission("frconomy.money.name")) {
+                OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+                if (main.getAccounts().hasAccount(target)) {
+                    Account account = main.getAccounts().getAccount(target);
+                    sender.sendMessage(main.getMoneyPlayer().replace("%money%", main.formatCurrency(account.getBalance())).replace("%name%", target.getName()));
+                    return true;
+                } else {
+                    sender.sendMessage("§cThe account was not found!");
+                }
+            } else {
+                sender.sendMessage(main.getPermission());
+            }
+        }
+        return false;
+    }
+
+}
---
+++ src/main/java/ro/fr33styler/frconomy/command/Pay.java
@@ -0,0 +1,68 @@
+package ro.fr33styler.frconomy.command;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import ro.fr33styler.frconomy.FrConomy;
+import ro.fr33styler.frconomy.account.Account;
+
+public class Pay implements CommandExecutor {
+
+    private FrConomy main;
+
+    public Pay(FrConomy main) {
+        this.main = main;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String name, String[] args) {
+        if (!sender.hasPermission("frconomy.pay")) {
+            sender.sendMessage(main.getPermission());
+        } else if (sender == Bukkit.getConsoleSender()) {
+            sender.sendMessage("Can be used only by players!");
+        } else if (args.length != 2) {
+            sender.sendMessage("§cInvalid arguments! /pay <name> <amount>");
+        } else {
+            Player player = (Player) sender;
+            OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+            if (player.getUniqueId() == target.getUniqueId()) {
+                sender.sendMessage(main.getMoneyYourself());
+            } else {
+                try {
+                    double amount = Math.abs(Double.parseDouble(args[1]));
+                    if (amount < 0) {
+                        sender.sendMessage(main.getPositive());
+                    } else if (!main.getAccounts().hasAccount(target)) {
+                        sender.sendMessage(main.getPayAccount());
+                    } else {
+                        Account playerAccount = main.getAccounts().getAccount(player);
+                        if (playerAccount.getBalance() < amount) {
+                            sender.sendMessage(main.getNotEnough());
+                        } else {
+                            Account targetAccount = main.getAccounts().getAccount(target);
+                            if (!targetAccount.canReceive()) {
+                                sender.sendMessage(main.getMoneyDenied());
+                            } else {
+                                playerAccount.setBalance(playerAccount.getBalance() - amount);
+                                targetAccount.setBalance(targetAccount.getBalance() + amount);
+                                sender.sendMessage(main.getSentTo().replace("%money%", main.formatCurrency(amount)).replace("%name%", target.getName()));
+                                if (target.isOnline()) {
+                                    ((Player) target).sendMessage(main.getReceivedFrom().replace("%money%", main.formatCurrency(amount)).replace("%name%", player.getName()));
+                                }
+                                return true;
+                            }
+                        }
+                    }
+                } catch (NumberFormatException e) {
+                    sender.sendMessage(main.getPayNotNumber());
+                }
+            }
+        }
+        return false;
+    }
+
+}
